4ca8bef06e50813299b20a13923691f58d77c02c
Abd-El-Rahman-Mohamed <90394194+Abd-El-Rahman-Mohamed@users.noreply.github.com>
2025-08-04 20:11:38 +0300

    Create clean_gospider_urls.sh

---
+++ clean_gospider_urls.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sed -i '' \
+    -e 's/^\[url\] - \[code-200\] - //' \
+    -e 's/^\[subdomains\] - //' \
+    -e 's/^\[href\] - //' \
+    -e 's/^\[javascript\] - //' \
+    -e 's/^\[form\] - //' \
+    -e 's/\[linkfinder\].-.\[from: //g' \
+    -e 's/\[linkfinder\].- //g' \
+    -e 's/\].*$//g' \
+    example_com
+
+awk '!/^http/' example_com
